8a4828e2cedb87f48cfaa98e94fe3e98451485f3
Phoebe <phoebe.wilckens@gmail.com>
2021-09-29 20:02:41 -0300

    update style and js

--- app.js
+++ app.js
@@ -4,29 +4,34 @@ let selectFrom = document.getElementById("currency-from");
 let selectTo = document.getElementById("currency-to");
 
 axios.get(apiCurrencies).then(function (response) {
-    let data = response.data;
-    for (const key in data) {
-        let opt = document.createElement("option");
-        opt.value = key;
-        opt.innerHTML = key;
-        selectFrom.append(opt);
-        let opt1 = document.createElement("option");
-        opt1.value = key;
-        opt1.innerHTML = key;
-        selectTo.append(opt1);
-    }
-})
+  let data = response.data;
+  for (const key in data) {
+    let opt = document.createElement("option");
+    opt.value = key;
+    opt.innerHTML = key;
+    selectFrom.append(opt);
+    let opt1 = document.createElement("option");
+    opt1.value = key;
+    opt1.innerHTML = key;
+    selectTo.append(opt1);
+  }
+});
 
 function calculate(event) {
-    event.preventDefault();
-    let key = config.KEY;
-    let quantity = document.getElementById("value").value;
-    let apiURL = `https://v6.exchangerate-api.com/v6/${key}/pair/${selectFrom.value}/${selectTo.value}/${quantity}`;
+  event.preventDefault();
+  let key = config.KEY;
+  let quantity = document.getElementById("value").value;
+  let apiURL = `https://v6.exchangerate-api.com/v6/${key}/pair/${selectFrom.value}/${selectTo.value}/${quantity}`;
+  if (quantity.length > 0) {
     axios.get(apiURL).then(function (response) {
-        console.log(response);
-        let result = document.getElementById("result");
-        result.innerHTML = `From ${selectFrom.value} to ${selectTo.value} are ${Math.round(response.data.conversion_result)}`;
-    })
+      let result = document.getElementById("result");
+      result.innerHTML = `From ${selectFrom.value} to ${
+        selectTo.value
+      } are ${new Intl.NumberFormat("de-DE").format(
+        response.data.conversion_result
+      )}`;
+    });
+  }
 }
 
 let form = document.querySelector("#form-currency");
